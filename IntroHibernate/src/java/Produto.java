a8642b70eeae8192f39a07f1596a457c22179936
Rafaelhss <rafaelhss@gmail.com>
2017-04-20 22:39:35 -0300

    projeto inicial com save da classe Projeto

---
+++ IntroHibernate/src/java/Produto.java
@@ -0,0 +1,50 @@
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Rafael.Soares
+ */
+@Entity
+public class Produto {
+    @Id
+    private String nome;
+    
+    private float preco;
+    
+    private String fabricante;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public float getPreco() {
+        return preco;
+    }
+
+    public void setPreco(float preco) {
+        this.preco = preco;
+    }
+
+    public String getFabricante() {
+        return fabricante;
+    }
+
+    public void setFabricante(String fabricante) {
+        this.fabricante = fabricante;
+    }
+    
+    
+    
+}
