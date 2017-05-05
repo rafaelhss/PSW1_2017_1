cf4dc0b050dbc70965538704bf27e678b84ef0c7
Rafaelhss <rafaelhss@gmail.com>
2017-05-04 22:33:44 -0300

    Add files via upload

---
+++ CadastroDeProfessores/src/java/Professor.java
@@ -0,0 +1,49 @@
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Rafael.Soares
+ */
+@Entity
+public class Professor {
+   
+   @Id
+   private String nome;
+   private String sobrenome;
+   private String disciplina;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getDisciplina() {
+        return disciplina;
+    }
+
+    public void setDisciplina(String disciplina) {
+        this.disciplina = disciplina;
+    }
+    
+   
+    
+}
---
+++ CadastroDeProfessores/src/java/cadastrarProfessor.java
@@ -0,0 +1,103 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+/**
+ *
+ * @author Rafael.Soares
+ */
+public class cadastrarProfessor extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            
+            Professor prof = new Professor();
+            
+            prof.setDisciplina(request.getParameter("disciplina"));
+            prof.setNome(request.getParameter("nome"));
+            prof.setSobrenome(request.getParameter("sobrenome"));
+            
+            Transaction tx = null;
+            try{
+                Session sessao = HibernateUtil.getSessionFactory().openSession();
+                tx = sessao.beginTransaction();
+                
+                sessao.save(prof);
+                sessao.flush();
+                
+                tx.commit();
+                sessao.close();
+                out.println("Professor Salvo com sucesso.");
+                        
+            } catch (Exception e) {
+                if(tx != null){
+                    tx.rollback();
+                }
+                e.printStackTrace();
+                out.println("Erro ao cadastrar Professor: " + e.getMessage());
+            }
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
